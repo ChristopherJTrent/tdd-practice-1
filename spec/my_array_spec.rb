27c124941a02f7ffb9b722492b61fe65f1069739
ChristopherJtrent <ChristopherJtrent@outlook.com>
2024-02-02 12:39:35 -0800

    implement specs for Array#two_sum and my_transpose

--- spec/my_array_spec.rb
+++ spec/my_array_spec.rb
@@ -11,4 +11,21 @@ describe 'my_array' do
             expect(my_uniq(r1)).to eq(r1_unique)
         end
     end
+    describe 'Array#two_sum' do
+        it 'finds all pairs of indices where the elements at those indices sum to zero' do 
+            arr_1 = [-1, 0, 2, -2, 1]
+            arr_1_two_sum = [[0, 4], [2, 3]]
+            expect(arr_1.two_sum).to eq(arr_1_two_sum)
+        end
+    end 
+    describe 'my_transpose' do
+        it 'converts between row-oriented and column-oriented matrix representations' do
+            matrix = [
+                [0, 1, 2],
+                [3, 4, 5],
+                [6, 7, 8]
+            ]
+            expect (my_transpose(matrix)).to eq(matrix.transpose)
+        end
+    end
 end
